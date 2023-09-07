ea924faf610df9142be2f5c01921718cf6c1ad6b
NikcWill <nwtads03@gmail.com>
2023-09-06 21:26:27 -0300

    js

---
+++ Aula10/script.js
@@ -0,0 +1,12 @@
+
+let texto = 'texto';
+let inteiro = 01;
+let decimal = 1.2;
+let booleano = true;
+let lista = ['a', 'q', 'q', 101, 103, ['12', 33]];
+let objeti = {
+    'nome': 'caio',
+    'email': 'w@gmail.com'};
+let nulo = null;
+let indefinido = undefined;
+
