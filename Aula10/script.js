ac3dd9d36a3012be146473ab9f93829cfa5c0353
NikcWill <nwtads03@gmail.com>
2023-09-13 21:38:46 -0300

    treinando js

--- Aula10/script.js
+++ Aula10/script.js
@@ -1,5 +1,5 @@
 
-let texto = 'texto';
+/* let texto = 'texto';
 let inteiro = 01;
 let decimal = 1.2;
 let booleano = true;
@@ -8,5 +8,92 @@ let objeti = {
     'nome': 'caio',
     'email': 'w@gmail.com'};
 let nulo = null;
-let indefinido = undefined;
+let indefinido = undefined; */
+
+/* let x = 10;
+let y = '10';
+
+console.log(x==y);
+
+function soma(a,b){
+    return a + b
+} */
+
+/* console.log(soma(5,5))
+
+alert(soma(10,20)) */
+
+/* let soma = (a,b)=> a+b
+    console.log(soma(5,5))
+
+
+let soma = (a,b)=>{
+    console.log('estou aqui');
+    return a + b
+} */
+
+/* let objetoarrow = {
+    'nome': 'Pedro',
+    'metodo': function(){
+        console.log(this.nome);
+    }
+}
+
+objetoarrow.metodo()
+
+let objeto = {
+    'nome': 'Pedro',
+    'metodo': ()=>{
+        console.log(this.nome)
+    }
+} */
+
+
+/* let nome = prompt('nome?')
+let idade = prompt('idade?')
+let altura = prompt('altura?')
+
+function Show(){
+    alert(`Seu nome é: ${nome} Sua idade é: ${idade} Sua altura é: ${altura}`)
+    
+}
+
+Show(); */
+
+/* 
+for (let x = 0; x<10; x++){
+    console.log(x)
+} */
+/* 
+let lista = ['maça', 'pera', 'uva']
+
+for(let x=0; x< lista.length ; x++){
+    console.log(lista[x]);
+} */
+
+/* let x =1;
+let y =2;
+let z =1;
+
+if(x==z && x==z || z ==x ){
+    console.log('x é igual a z');
+}else {
+    console.log('x é diferente a z');
+
+}
+ */
+
+/* veiculos = [
+    ['Ford', 4, true],
+    ['Volvo', 5, false],
+    ['ferrari', 2, true],
+    ['tesla', 4, false],
+    ['Corsa', 1, true],
+]
+
+console.log(veiculos.length); */
+ 
+ for (let x = 0; x < veiculos.length  ; x++){
+    console.log('nome');
+} 
 
