270dda06364c5f7d2b837d837a8209e57b20202b
NikcWill <nwtads03@gmail.com>
2023-09-14 21:36:24 -0300

    map filter

--- Aula10/script.js
+++ Aula10/script.js
@@ -86,14 +86,96 @@ if(x==z && x==z || z ==x ){
 /* veiculos = [
     ['Ford', 4, true],
     ['Volvo', 5, false],
-    ['ferrari', 2, true],
-    ['tesla', 4, false],
+    ['Ferrari', 2, true],
+    ['Tesla', 4, false],
     ['Corsa', 1, true],
-]
+];
 
-console.log(veiculos.length); */
+let nomes = [];
+let quantPessoas = [];
+let vagas = [];
+let estacionado= [];
+
+console.log(veiculos.length);
+
+for (let x = 0; x < veiculos.length; x++) {
+    console.log(veiculos[x][0].toUpperCase());
+    nomes.push(veiculos[x][0].toUpperCase());
+    vagas.push(x+5);
+
+    if(veiculos[x][1]>= 3){
+        quantPessoas.push(veiculos[x][0].toUpperCase())
+    }
+
+    if(veiculos[x][2]){
+        estacionado.push(veiculos[x][0].toUpperCase())
+    }
+
+}
+
+function Show(){
+    alert(`Lista dos nome de todos os carros:\n${nomes}\n 
+    Lista com todos os carro que cabem mais ou igual do que 3 passageiros:\n${quantPessoas}\n 
+    Lista número da vaga que cada carro está estacionado:\n${vagas}\n 
+    Lista com todos os carro estacionados:\n${estacionado}`)
+    
+}
+
+Show();
+
+console.log(nomes);
+console.log(quantPessoas);
+console.log(vagas);
+console.log(estacionado); */
+
+/* let lista = [1,2,3,4,5,6,7,8,9,10]
+let quadrado = lista.map((numero)=> numero*numero)
+console.log(quadrado); */
+
+/* let produto = ['carro', 'moto', 'bicicleta', 'batera'];
+let filtro = produto.filter((produto)=>produto === 'moto');
+
+let lista = ['carro', 'moto', 'bicicleta', 'batera'];
+let novoNome = lista.map((produto)=>produto + ' Novo');
+
+console.log(novoNome);
+console.log(filtro); */
+
+/* let maiusculo = veiculos.map((nome)=>nome[0].toUpperCase());
+
+let passageiros = veiculos.filter((carro)=>carro[1] > 3).map((nomeCarro)=>nomeCarro[0]);
+
+let estacionado = veiculos.filter((carros)=>carros[2] == true).map((nomeCarro)=>nomeCarro[0]);
+
  
- for (let x = 0; x < veiculos.length  ; x++){
-    console.log('nome');
-} 
+
+console.log(maiusculo);
+
+console.log(passageiros);
+
+console.log(estacionado); */
+
+const students = [
+    {nome: 'Alice', nota: 85},
+    {nome: 'Bob', nota: 72},
+    {nome: 'Charlie', nota: 90},
+    {nome: 'Dacid', nota: 65},
+    {nome: 'Eve', nota: 88},
+];
+
+// Usando o método map para extrair todas as notas
+const notas = students.map(estudante => estudante.nota);
+
+    console.log(notas); 
+
+
+    const soma = notas.reduce((acumulador, nota) => {
+        return acumulador + nota;
+    }, 0);
+
+let media = soma/ students.length;
+console.log(media)
+
+const filtro = students.filter((estudante) => estudante.nota > media).map(nomeAluno => nomeAluno.nome);
+console.log('Alunos com notas acima da média:', filtro);
 
